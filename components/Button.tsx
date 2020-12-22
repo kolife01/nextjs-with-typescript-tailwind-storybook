21bdcdfb9aba07aece373495f6d11c1116de0f97
Kohei Tanaka <kohei69@gmail.com>
2020-12-22 21:40:09 +0900

    add storybook with tailwind

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,44 +1,43 @@
 import React from 'react';
-import './button.css';
 
 export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button: React.FC<ButtonProps> = ({
   primary = false,
-  size = 'medium',
+  size = "medium",
   backgroundColor,
   label,
   ...props
 }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
+  const baseButton = "rounded-full font-bold";
+  const sizeMode =
+    size === "small"
+      ? "py-1.5 px-4 text-xs"
+      : size === "medium"
+      ? "py-2 px-5 text-sm"
+      : size === "large"
+      ? "py-3 px-6 text-base"
+      : "";
+  return primary ? (
+    <div>
+      <button
+        type="button"
+        className={`text-white bg-blue-450 ${baseButton} ${sizeMode}`}
+        {...props}
+      >
+        {label}
+      </button>
+    </div>
+  ) : (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={`text-gray-600 bg-transparent shadow-inner ${baseButton} ${sizeMode}`}
       style={{ backgroundColor }}
       {...props}
     >
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          450: '#1ea7fd'
+        },
+      },
+    },
+    fontFamily: {
+      'sans': ['Nunito Sans', 'Helvetica Neue', "Helvetica", "Arial", "sans-serif"],
+    }
   },
   variants: {
     extend: {},
